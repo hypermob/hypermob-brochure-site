433c21afd8bb1e3b02673d80f0a22f1a541b6482
pikax <carlos@hypermob.co.uk>
2019-08-06 10:50:48 +0100

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="app">
+    <nuxt class="content" />
+    <footer>
+      Hypermob Ltd is a company registered in England under number 12108411. 
+    </footer>
+  </div>
+</template>
+
+<style>
+html {
+  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
+    Roboto, 'Helvetica Neue', Arial, sans-serif;
+  font-size: 16px;
+  word-spacing: 1px;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
+}
+
+.content {
+  flex: 1 1 auto;
+}
+.app {
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+}
+
+footer {
+  /* font-size: 0.5em; */
+  /* color: #bbb; */
+  font-weight: 300;
+  margin: 0 auto;
+}
+
+*,
+*:before,
+*:after {
+  box-sizing: border-box;
+  margin: 0;
+}
+
+.button--green {
+  display: inline-block;
+  border-radius: 4px;
+  border: 1px solid #3b8070;
+  color: #3b8070;
+  text-decoration: none;
+  padding: 10px 30px;
+}
+
+.button--green:hover {
+  color: #fff;
+  background-color: #3b8070;
+}
+
+.button--grey {
+  display: inline-block;
+  border-radius: 4px;
+  border: 1px solid #35495e;
+  color: #35495e;
+  text-decoration: none;
+  padding: 10px 30px;
+  margin-left: 15px;
+}
+
+.button--grey:hover {
+  color: #fff;
+  background-color: #35495e;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="container">
+    <div>
+      <logo />
+      <h2 class="subtitle">Solving your business problems using our programming skills</h2>
+      <p class="description">
+        If you need to bring your legacy codebase to the nextgen or build
+        nextgen from scratch we can help.
+      </p>
+
+      <p class="specific">We specialise in dotnet core, Typescript, VueJS and NodeJS.</p>
+      <p>Based in London, UK</p>
+
+      <div class="links">
+        <a href="mailto:contact@hypermob.co.uk" target="_blank" class="button--green">Contact</a>
+        <a href="https://github.com/hypermob" target="_blank" class="button--grey">GitHub</a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+
+export default {
+  components: {
+    Logo
+  }
+}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  /* min-height: 100vh; */
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 30px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.description {
+  max-width: 24em;
+  margin: 0 auto;
+}
+
+.specific {
+  margin: 1em auto;
+  color: #666;
+  max-width: 27em;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
